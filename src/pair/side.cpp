464cdfdf53e6f362055e5db29a696ae52b6ccc0a
Xiaoyu Chen <xiachen@emi.u-bordeaux.fr>
2023-02-21 16:56:06 +0100

    Création de la partie pairage du projet.

---
+++ src/pair/side.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <string>
+#include "../common/kepInstance.h"
+#include "../common/kepSolution.h"
+#include "../common/kepTest.h"
+using namespace std;
+
+
+
+void hi()
+{
+  cout << "hi\n";
+}
