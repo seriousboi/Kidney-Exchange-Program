aaa9c9638f75431b3161cafb9e1f3811b6d5dae6
seriousboi <ppinet9@gmail.com>
2023-05-15 13:34:40 +0200

    Nettoyage.

--- build/pair/comp.sh
+++
@@ -1,8 +0,0 @@
-rm pairkep.exe
-
-g++ -c  ../../src/pair/side.cpp ../../src/common/kepTest.cpp ../../src/common/kepInstance.cpp ../../src/common/kepSolution.cpp ../../src/pair/main.cpp
-g++ side.o kepInstance.o kepSolution.o kepTest.o main.o -o pairkep.exe
-
-rm kepInstance.o kepSolution.o kepTest.o main.o side.o
-
-./pairkep.exe
